35091683f4fc2f02875bffc38b3fe7a4c0e6820b
DraganMax <maksims.dragans@gmail.com>
2019-08-22 20:58:42 +0300

    TypesAndVariable

---
+++ TypesAndVariables/Exercise 7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a string: ");
+            Uppercase();
+            Console.Read(); 
+        }
+
+        private static void Uppercase()
+        {
+            string text = Console.ReadLine();
+            int count = 0;
+            foreach (char a in text)
+            {
+                if (char.IsUpper(a))
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+
+        }
+    }
+}
---
+++ TypesAndVariables/Exercise 8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number of minutes and you will get that number in days and years:");
+            int userInput = int.Parse(Console.ReadLine());
+            NumberOfDaysAndYears(userInput);
+            Console.Read();
+        }
+
+        static void NumberOfDaysAndYears(double minutes)
+        {
+            var minutesInDays = minutes * 0.000694444;
+            var minutesInYears = minutes * 0.000001902587519;
+            Console.WriteLine($"This are minutes in days: {minutesInDays:0.00} days.");
+            Console.WriteLine($"This are minutes in years: {minutesInYears:0.00} years.");
+            
+
+        }
+    }
+}
